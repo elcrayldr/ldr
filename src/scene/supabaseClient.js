79976df94a9dcec2044b7d47c139a2eb0d2de65d
elcrayldr <lazetaestudios@gmail.com>
2025-10-26 16:03:57 +0100

    Update supabaseClient.js

--- src/scene/supabaseClient.js
+++ src/scene/supabaseClient.js
@@ -1,8 +1,8 @@
 // src/supabaseClient.js
 import { createClient } from "https://esm.sh/@supabase/supabase-js@2";
 
-export const SUPABASE_URL = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imttbm93bHd5b2R0ZHdyc2plYWlzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjE0NTc2MTYsImV4cCI6MjA3NzAzMzYxNn0.PotpYnRiu7lj1Hs1pQa8qeuI13ryN1lJWnXDQGd-6hc";
-export const SUPABASE_ANON_KEY = "YOUR-ANON-KEY";
+export const SUPABASE_URL = "https://kmnowlwyodtdwrsjeais.supabase.co";
+export const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imttbm93bHd5b2R0ZHdyc2plYWlzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjE0NTc2MTYsImV4cCI6MjA3NzAzMzYxNn0.PotpYnRiu7lj1Hs1pQa8qeuI13ryN1lJWnXDQGd-6hc";
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY, {
   auth: { persistSession: false }
